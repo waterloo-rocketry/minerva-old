0e33ec14fe198328f2d43cf0e64ada76ccf9f5d9
Chris Sankey <csankey6@gmail.com>
2020-06-29 21:40:30 -0400

    Add help command

--- functions/handlers/command-handler.js
+++ functions/handlers/command-handler.js
@@ -5,6 +5,9 @@ const slack_handler = require('./slack-handler');
 
 module.exports.process = async function (request) {
     slack_handler.postMessageToChannel(request.user_name + " executed " + request.command + " " + request.text , "minerva-log");
+    if(request.text.startsWith("help")) {
+        return Promise.resolve("For help, see: https://github.com/waterloo-rocketry/minerva")
+    }
     if (request.command === "/notify") {
         return notify_command.send(request.user_id, request.text, request.channel_id);
     } else if (request.command === "/agenda") {
