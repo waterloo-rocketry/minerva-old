c278f6a0afa199d5ff2f1646a861f4772646bf4a
Chris Sankey <csankey6@gmail.com>
2020-08-25 23:04:22 -0400

    Make logging look nicer

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
 const functions = require("firebase-functions");
-const commands = require("./handlers/command-handler");
 const slack = require("./handlers/slack-handler");
 const events = require("./scheduled/events");
+
 // prettier-ignore
 exports.slack_commands = functions.https.onRequest((request, response) => {
     response.status(200).send("Command recieved");
+
     // handle requests that have do not originate from slack? i.e if request has no body
-    commands.process(request.body).then(result => {
+    require("./handlers/command-handler").process(request.body).then(result => {
         if (result !== undefined && result != "") {
             slack.postEphemeralMessage(result, request.body.channel_name, request.body.user_id);
         }
     }).catch(error => {
         console.log(error);
-        console.log(JSON.stringify(error));
-        slack.postMessageToChannel(JSON.stringify(error), "minerva-log", false);
+        slack.postMessageToChannel("```" + JSON.stringify(error, null, 4) + "```", "minerva-log", false);
+
         //If there's an error sending this message, well, the bot just won't respond, in which case you know theres something wrong.
         slack.postEphemeralMessage(
             "Command failed: " + error + "\nSee: https://github.com/waterloo-rocketry/minerva for help with commands.",
@@ -27,8 +28,8 @@ exports.slack_commands = functions.https.onRequest((request, response) => {
 // Run on the 25th and 55th minute of every hour since events start on X:30 or X:00 and we want to alert 5 mins or so ahead
 // We can specify a timezone, but in this case it does not matter. Default is Pacific time which has the same minute # as Eastern (only hours are changed)
 // prettier-ignore
-exports.event_check = functions.pubsub.schedule("25,55 * * * *").timeZone("America/New_York").onRun(context => {events
-    .checkForEvents()
+exports.event_check = functions.pubsub.schedule("25,55 * * * *").timeZone("America/New_York").onRun(context => {
+    events.checkForEvents()
     .then(() => {
         // do nothing
     })
@@ -38,4 +39,9 @@ exports.event_check = functions.pubsub.schedule("25,55 * * * *").timeZone("Ameri
         slack.postMessageToChannel("Error with upcoming meeting:\n" + error, "minerva-log", false);
     });
     return "Ran meeting check";
+});
+
+exports.interactivity = functions.https.onRequest((request, response) => {
+    console.log(request);
+    response.status(200).send("Recieved");
 });
