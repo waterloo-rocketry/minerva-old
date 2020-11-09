d1ec1e22bdf80caf4b214440f34217735fc01f58
Chris Sankey <csankey6@gmail.com>
2020-11-09 17:10:04 -0500

    Move error handling to error handler

--- functions/commands/meeting.js
+++ functions/commands/meeting.js
@@ -1,13 +1,9 @@
 module.exports.send = async function (userId, textParams, originChannelID, originChannelName, trigger) {
-    try {
-        if (textParams.startsWith("reminder")) {
-            return require("./meeting/reminder").send(userId, textParams.replace("reminder ", ""), originChannelName);
-        } else if (textParams.startsWith("edit")) {
-            return require("./meeting/edit").send(userId, textParams.replace("edit ", ""), originChannelID, originChannelName, trigger);
-        } else {
-            return Promise.reject("Incorrect usage: /reminder <reminder/edit>");
-        }
-    } catch (error) {
-        return Promise.reject(error);
+    if (textParams.startsWith("reminder")) {
+        return require("./meeting/reminder").send(userId, textParams.replace("reminder ", ""), originChannelName);
+    } else if (textParams.startsWith("edit")) {
+        return require("./meeting/edit").send(userId, textParams.replace("edit ", ""), originChannelID, originChannelName, trigger);
+    } else {
+        return Promise.reject("Incorrect usage: /reminder <reminder/edit>");
     }
 };
