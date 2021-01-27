104f32fe57a75c0dcc017d7134edc53c0af5e696
Wendi <wendi.yu@yahoo.ca>
2021-01-27 16:45:09 -0700

    Be more strict on defaults

--- src/commands/meeting.js
+++ src/commands/meeting.js
@@ -2,7 +2,9 @@ module.exports.send = async function (userId, textParams, originChannelID, origi
     if (textParams.startsWith("reminder")) {
         return require("./meeting/reminder").send(originChannelID);
     // the "edit" path is the default if no textParam is provided
-    } else {
+    } else if (textParams.startsWith("edit") || textParams.trim().length === 0){
         return require("./meeting/edit").send(originChannelID, trigger);
-    } 
+    } else {
+        return Promise.reject("Incorrect usage: /meeting <reminder/edit>");
+    }
 };
