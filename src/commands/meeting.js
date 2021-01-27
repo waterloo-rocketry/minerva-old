9d9723cc7f786aa3a230355eb03b69060a6189c6
Wendi <wendi.yu@yahoo.ca>
2021-01-27 00:13:43 -0700

    Make meeting command take default as edit

--- src/commands/meeting.js
+++ src/commands/meeting.js
@@ -1,9 +1,8 @@
 module.exports.send = async function (userId, textParams, originChannelID, originChannelName, trigger) {
     if (textParams.startsWith("reminder")) {
         return require("./meeting/reminder").send(originChannelID);
-    } else if (textParams.startsWith("edit")) {
-        return require("./meeting/edit").send(originChannelID, trigger);
+    // the "edit" path is the default if no textParam is provided
     } else {
-        return Promise.reject("Incorrect usage: /meeting <reminder/edit>");
-    }
+        return require("./meeting/edit").send(originChannelID, trigger);
+    } 
 };
