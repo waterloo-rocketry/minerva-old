01f79c97b573f4ed472b15fa9aac109a3bcbe043
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2022-02-20 13:00:04 -0500

    Remove single channel DM from meeting reminder command

--- src/commands/meeting/reminder.js
+++ src/commands/meeting/reminder.js
@@ -17,21 +17,4 @@ module.exports.send = async function (originChannelId) {
     const message = await events.generateMessage(event, parameters, timeDifference, isEventSoon, startTimeDate, emojiPair);
 
     await slack_handler.postMessageToChannel(message, parameters.mainChannel, false);
-
-    if (parameters.alertType === "alert-single-channel") {
-        console.log("[DEBUG] Alert type is 'alert-single-channel'")
-        // when we alert single channel guests we simply want to PM them the message
-        const responses = await slack_handler.directMessageSingleChannelGuestsInChannels(
-            message +
-                "\n\n_You have been sent this message because you are a single channel guest who might have otherwise missed this alert.\n\n" +
-                "If you're unsure what this message is about, feel free to message the original poster for more information._",
-            parameters.channels
-        );
-
-        responses = await responses;
-        console.log("[DEBUG] Direct Message Responses:" + responses)
-
-
-        return responses.length + " single-channel-guests messaged.";
-    }
 };
